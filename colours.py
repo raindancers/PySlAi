0e2b9599504ddfdcf1e5264f9d3da6156885879d
Andrew <andrew.frazer@raindancers.cloud>
2024-06-30 17:40:57 +1200

    Create colours.py

---
+++ colours.py
@@ -0,0 +1,11 @@
+# https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797#8-16-colors
+class colours:
+    HEADER = '\033[34m'  #blue
+    CLAUDE = '\033[36m' # cycan
+    YOU = '\033[32m'  # green 
+    ME = '\033[37m'   #white
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
